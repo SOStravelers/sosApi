9d5bf24ae17ddfee6ef5eca5073b4af96e92ce7b
JohannSch <jschacosta@gmail.com>
2023-11-16 15:06:35 +0100

    500mb global fotos

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ db.on("error", (err) => {
 const app = express();
 const router = Router();
 app.use(express.static(staticDir));
-app.use(express.json({ limit: "1000mb" }));
+app.use(express.json({ limit: "500mb" }));
 
 //Nos sirve para pintar las peticiones HTTP request que se solicitan a nuestro aplicación.
 app.use(morgan("tiny"));
