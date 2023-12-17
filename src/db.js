b641e24bf838e5000ab0597ef25f1cdd7197fe6d
JohannSch <jschacosta@gmail.com>
2023-12-17 01:14:39 -0300

    cambio data base

--- src/db.js
+++ src/db.js
@@ -13,11 +13,11 @@ const dbConfig = {
 
 const url = "mongodb://localhost:27017/sosLocal";
 const env = process.env.NODE_ENV || "";
-// console.log(`Conectando... ${dbConfig[env]}`);
-console.log(`Conectando... ${url}`);
+console.log(`Conectando... ${dbConfig[env]}`);
+// console.log(`Conectando... ${url}`);
 mongoose.set("strictQuery", false);
-// mongoose.connect(dbConfig[env], {
-mongoose.connect(url, {
+mongoose.connect(dbConfig[env], {
+  // mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useFindAndModify: false,
