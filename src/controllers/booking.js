ed0f02a9973d496194caec0d888a03d7a0ad37e9
Yapy <povedayesid002@gmail.com>
2023-12-21 18:07:45 -0500

    👔  added controllers, routes for booking

--- src/controllers/booking.js
+++ src/controllers/booking.js
@@ -6,13 +6,14 @@ import { sendEmailPaymentConfirmation } from "../services/aws_ses.js";
 export const create = async (req, res, next) => {
   global.logger.info("---CREATE NEW BOOKING---");
   try {
+    const emailData = req.body.emailData;
     const bookingData = req.body;
-    const email = req.body.email;
+    bookingData.emailData = null;
     let booking = new Booking(bookingData);
     let query = {
       $and: [
         {
-          businessUser: booking.location,
+          location: booking.location,
         },
         {
           subService: booking.subService,
@@ -23,9 +24,6 @@ export const create = async (req, res, next) => {
         {
           startTime: booking.startTime,
         },
-        {
-          endTime: booking.startTime,
-        },
       ],
     };
     let exists = await Booking.findOne(query, {}).exec();
@@ -33,8 +31,8 @@ export const create = async (req, res, next) => {
       throw createError(409, "document already exists");
     } else {
       const newBooking = await booking.save();
-      if (email) sendEmailPaymentConfirmation(email);
-      return res.status(201).json({ booking: newBooking, msg: "new Document" });
+      if (emailData) sendEmailPaymentConfirmation(emailData);
+      res.status(201).json({ booking: newBooking, msg: "new Document" });
     }
   } catch (err) {
     next(err);
@@ -59,17 +57,17 @@ export const getBookings = async (req, res, next) => {
     Object.assign(body, req.query);
     const populate = [
       {
-        path: "hotel",
+        path: "businessUser",
         //   select: "isActive name  email phone creator user imgUrl emails type",
       },
       {
-        path: "worker",
+        path: "workerUser",
       },
       {
-        path: "client",
+        path: "clientUser",
       },
       {
-        path: "creator",
+        path: "creatorUser",
       },
     ];
     let options = {
@@ -113,3 +111,632 @@ export const updateOne = async (req, res, next) => {
     next(err);
   }
 };
+/*  get day by client  */
+
+export const getDayClientId = async(req, res, next) =>{
+
+  global.logger.info("---GET CLIENT BOOKING---")
+  
+  const user = req.user
+
+  const populate = [
+    {
+      path: 'businessUser',
+      select: 'businessData personalData'
+    },
+    {
+     path: 'workerUser',
+     select: 'workerData personalData'
+    }, 
+    {
+      path: 'service',
+      select: 'name isActive coverImg'
+    },
+    {
+      path: 'subservice',
+      select: 'name isActive coverImg duration'
+    },
+    {
+      path: 'clientUser',
+      select: 'personalData'
+    }
+  ]
+  
+  try {
+   
+  let options = {
+
+     populate,
+     select: 'startTime endTime date duration',
+     page: req.query.page || 1,
+     limit: req.query.limit || 2,
+     sort: { updatedAt: -1 },
+  }
+
+  let query = {
+     "date.stringData": req.query.date,
+    "clientUser": user._id.toString(),
+  }
+  
+   const booking = await Booking.paginate(query, options);
+   
+   if (!booking) throw createError(404, "Booking not found");
+   res.status(200).json(booking);
+   
+
+ 
+ } catch (err) {
+
+   next(err);
+ }
+
+}
+/* get month by client  */
+export const getMonthClientId = async(req, res, next) => {
+
+  global.logger.info("---GET CLIENT MONTH BOOKING---")
+  
+  const user = req.user
+
+  const populate = [
+    {
+      path: 'businessUser',
+      select: 'businessData personalData'
+    },
+    {
+     path: 'workerUser',
+     select: 'workerData personalData'
+    },
+    {
+      path: 'service',
+      select: 'name isActive coverImg'
+    },
+    {
+      path: 'subservice',
+      select: 'name isActive coverImg duration'
+    }, 
+    {
+      path: 'clientUser',
+      select: 'personalData'
+    }
+  ]
+
+  try {
+   
+  let options = {
+
+     populate,
+     select: 'startTime endTime date duration',
+     page: req.query.page || 1,
+     limit: req.query.limit || 8,
+     sort: { updatedAt: -1 },
+  }
+
+/* format: YY-MM-DD => 2024-12-23 */  
+
+const dateFromBody = req.query.date;
+
+const [year, month, dia] = dateFromBody.split("-");
+
+const startDate = new Date(year, month - 1, 1); 
+
+const endDate = new Date(year, month, 0);
+
+const query = {
+  "clientUser": user._id.toString(),
+  "date.isoDate": {
+    $gte: startDate,
+    $lte: endDate
+  }
+};
+
+   const booking = await Booking.paginate(query, options);
+   
+   if (!booking) throw createError(404, "Booking not found");
+   res.status(200).json(booking);
+   
+
+ 
+ } catch (err) {
+
+   next(err);
+ }
+
+
+}
+
+/*  get all bookings for client  */
+export const getAllClientsId = async(req, res, next) => {
+
+  /* bug sale el logger de una ruta diferente */
+  
+  global.logger.info("---GET TO CLIENT ALL BOOKING---")
+  
+  const user = req.user
+
+
+  const populate = [
+
+    {
+      path: 'businessUser',
+      select: 'businessData personalData'
+    },
+    {
+     path: 'workerUser',
+     select: 'workerData personalData'
+    },
+    {
+      path: 'service',
+      select: 'name isActive coverImg'
+    },
+    {
+      path: 'subservice',
+      select: 'name isActive coverImg duration'
+    },
+    {
+      path: 'clientUser',
+      select: 'personalData'
+    }
+  ]
+
+  try {
+   
+  let options = {
+
+     populate,
+     select: 'startTime endTime date duration',
+     page: req.query.page || 1,
+     limit: req.query.limit || 5,
+     sort: { updatedAt: -1 },
+  }
+
+  let query = {
+
+    "clientUser": user._id.toString()
+
+  }
+  console.log(user._id.toString())
+   const booking = await Booking.paginate(query, options);
+   
+   if (!booking) throw createError(404, "Booking not found");
+   res.status(200).json(booking);
+   
+ 
+ } catch (err) {
+
+   next(err);
+ }
+
+}
+
+/* get all workers */
+export const getAllworkersId = async(req, res, next) => {
+
+
+  global.logger.info("---GET TO WORKER ALL BOOKING---")
+  
+  const user = req.user
+
+  const populate = [
+    {
+      path: 'businessUser',
+      select: 'businessData personalData'
+    },
+    {
+     path: 'workerUser',
+     select: 'workerData personalData'
+    },
+    {
+      path: 'service',
+      select: 'name isActive coverImg'
+    },
+    {
+      path: 'subservice',
+      select: 'name isActive coverImg duration'
+    },
+    {
+      path: 'clientUser',
+      select: 'personalData'
+    }
+  ]
+
+  try {
+   
+  let options = {
+
+     populate,
+     select: 'startTime endTime date duration',
+     page: req.query.page || 1,
+     limit: req.query.limit || 8,
+     sort: { updatedAt: -1 },
+  }
+
+
+  let query = {
+
+    "workerUser": user._id.toString(),
+  
+  }
+  
+   const booking = await Booking.paginate(query, options);
+   
+   if (!booking) throw createError(404, "Booking not found");
+   res.status(200).json(booking);
+   
+
+ 
+ } catch (err) {
+
+   next(err);
+ }
+
+
+
+}
+
+/* get day by worker  */
+export const getDayWorkers = async (req, res, next) => {
+
+  global.logger.info("---GET DAYS TO WORKER BOOKING---")
+  
+  const user = req.user
+
+  const populate = [
+    {
+      path: 'businessUser',
+      select: 'businessData personalData'
+    },
+    {
+     path: 'workerUser',
+     select: 'workerData personalData'
+    },
+    {
+      path: 'service',
+      select: 'name isActive coverImg'
+    },
+    {
+      path: 'subservice',
+      select: 'name isActive coverImg duration'
+    },
+    {
+      path: 'clientUser',
+      select: 'personalData'
+    }
+  ]
+
+  try {
+   
+
+    const [year, month, day] = req.query.date.split("-");
+
+    console.log(day, month)
+    const startDate = new Date(year, month - 1, parseInt(day)); 
+    
+   
+    const endDate = new Date(year, month - 1, parseInt(day)+3);
+    
+    //  Sumar dos días a la fecha de fin
+    /*  endDate.setDate(endDate.getDate() + 2); */
+   
+
+  let options = {
+
+     populate,
+     select: 'startTime endTime date duration',
+     page: req.query.page || 1,
+     limit: req.query.limit || 8,
+     sort: { updatedAt: -1 },
+  }
+
+  let query = {
+    "workerUser": user._id.toString(),
+    "date.isoDate": {
+      $gte: startDate,
+      $lte: endDate
+    }
+  }
+  
+   const booking = await Booking.paginate(query, options);
+   
+   if (!booking) throw createError(404, "Booking not found");
+   res.status(200).json(booking);
+   
+
+ 
+ } catch (err) {
+
+   next(err);
+ }
+
+
+}
+
+/* get the business day for a specific place. */
+
+export const getDayBusiness = async (req, res, next) => {
+
+  global.logger.info("---GET SPECIFIC DATE TO BUSINESS BOOKING---")
+  
+  const user = req.user
+
+  const populate = [
+    {
+      path: 'businessUser',
+      select: 'businessData personalData'
+    },
+    {
+     path: 'workerUser',
+     select: 'workerData personalData'
+    },
+    {
+      path: 'service',
+      select: 'name isActive coverImg'
+    },
+    {
+      path: 'subservice',
+      select: 'name isActive coverImg duration'
+    },
+    {
+      path: 'clientUser',
+      select: 'personalData'
+    }
+  ]
+
+
+  try {
+   
+
+    const [year_start, month_start, day_start] = req.query.date_start.split("-");
+    const [year_end, month_end, day_end] = req.query.date_end.split("-");
+
+    const startDate = new Date(year_start, month_start - 1, day_start); 
+    const endDate = new Date(year_end, month_end - 1, day_end);
+
+
+  let options = {
+
+     populate,
+     select: 'startTime endTime date duration',
+     page: req.query.page || 1,
+     limit: req.query.limit || 8,
+     sort: { updatedAt: -1 },
+  }
+
+  let query = {
+    "workerUser": user._id.toString(),
+    "date.isoDate": {
+      $gte: startDate,
+      $lte: endDate
+    },
+    "status": { $in: ['canceled', 'completed', 'failed'] }
+  }
+  
+   const booking = await Booking.paginate(query, options);
+   
+   if (!booking) throw createError(404, "Booking not found");
+   res.status(200).json(booking);
+   
+
+ 
+ } catch (err) {
+
+   next(err);
+ }
+
+
+
+}
+
+/* get all business  */
+
+export const getAllBusiness = async (req, res, next) => {
+
+  global.logger.info("---GET ALL BUSINESS BOOKING---")
+  
+  const user = req.user
+
+  const populate = [
+    {
+      path: 'businessUser',
+      select: 'businessData personalData'
+    },
+    {
+     path: 'workerUser',
+     select: 'workerData personalData'
+    },
+    {
+      path: 'service',
+      select: 'name isActive coverImg'
+    },
+    {
+      path: 'subservice',
+      select: 'name isActive coverImg duration'
+    },
+    {
+      path: 'clientUser',
+      select: 'personalData'
+    }
+  ]
+
+  try {
+   
+    
+  let options = {
+
+     populate,
+     select: 'startTime endTime date duration',
+     page: req.query.page || 1,
+     limit: req.query.limit || 8,
+     sort: { updatedAt: -1 },
+  }
+
+  let query = {
+    "businessUser": user._id.toString(),
+    "status": { $in: ['canceled', 'completed', 'failed'] }
+  }
+  
+   const booking = await Booking.paginate(query, options);
+   
+   if (!booking) throw createError(404, "Booking not found");
+   res.status(200).json(booking);
+   
+
+ 
+ } catch (err) {
+
+   next(err);
+ }
+
+
+}
+
+/* get year business */
+
+export const getYearBusiness = async (req, res, next) => {
+
+  global.logger.info("---GET YEAR TO BUSINESS BOOKING---")
+  
+  const user = req.user
+
+  const populate = [
+    {
+      path: 'businessUser',
+      select: 'businessData personalData'
+    },
+    {
+     path: 'workerUser',
+     select: 'workerData personalData'
+    },
+    {
+      path: 'service',
+      select: 'name isActive coverImg'
+    },
+    {
+      path: 'subservice',
+      select: 'name isActive coverImg duration'
+    },
+    {
+      path: 'clientUser',
+      select: 'personalData'
+    }
+  ]
+
+  try {
+   
+
+    const [year, month, day] = req.query.date.split("-");
+
+
+    const startDate = new Date(year, 0, 1);
+
+    const endDate = new Date(year, 11, 31);
+    
+    
+  let options = {
+
+     populate,
+     select: 'startTime endTime date duration',
+     page: req.query.page || 1,
+     limit: req.query.limit || 8,
+     sort: { updatedAt: -1 },
+  }
+
+  let query = {
+    "businessUser": user._id.toString(),
+    "date.isoDate": {
+      $gte: startDate,
+      $lte: endDate
+    },
+    "status": { $in: ['canceled', 'completed', 'failed'] }
+  }
+  
+   const booking = await Booking.paginate(query, options);
+   
+   if (!booking) throw createError(404, "Booking not found");
+   res.status(200).json(booking);
+   
+
+ 
+ } catch (err) {
+
+   next(err);
+ }
+
+
+}
+
+/* get month business  */
+
+export const getMonthBusiness = async(req, res, next) => {
+
+
+  global.logger.info("---GET MONTH BUSINESS BOOKING---")
+  
+  const user = req.user
+
+  const populate = [
+    {
+      path: 'businessUser',
+      select: 'businessData personalData'
+    },
+    {
+     path: 'workerUser',
+     select: 'workerData personalData'
+    },
+    {
+      path: 'service',
+      select: 'name isActive coverImg'
+    },
+    {
+      path: 'subservice',
+      select: 'name isActive coverImg duration'
+    }, 
+    {
+      path: 'clientUser',
+      select: 'personalData'
+    }
+  ]
+
+  try {
+   
+  let options = {
+
+     populate,
+     select: 'startTime endTime date duration',
+     page: req.query.page || 1,
+     limit: req.query.limit || 8,
+     sort: { updatedAt: -1 },
+  }
+
+/* format: YY-MM-DD => 2024-12-23 */  
+
+const dateFromBody = req.query.date;
+
+const [year, month, day] = dateFromBody.split("-");
+
+const startDate = new Date(year, month - 1, 1); 
+
+const endDate = new Date(year, month, 0);
+
+const query = {
+  "businessUser": user._id.toString(),
+  "date.isoDate": {
+    $gte: startDate,
+    $lte: endDate
+  },
+  "status": { $in: ['canceled', 'completed', 'failed'] }
+};
+
+   const booking = await Booking.paginate(query, options);
+   
+   if (!booking) throw createError(404, "Booking not found");
+   res.status(200).json(booking);
+   
+
+ 
+ } catch (err) {
+
+   next(err);
+ }
+
+}
--- src/routes/booking.js
+++ src/routes/booking.js
@@ -4,6 +4,16 @@ import {
   getById,
   getBookings,
   updateOne,
+  getDayClientId,
+  getMonthClientId,
+  getAllClientsId,
+  getAllworkersId,
+  getDayWorkers,
+  getDayBusiness,
+  getAllBusiness,
+  getYearBusiness,
+  getMonthBusiness
+
 } from "../controllers/booking.js";
 import validateParams from "../middleware/validate.js";
 
@@ -24,6 +34,11 @@ router.post(
       //   type: "string",
       // },
       {
+        param_key: "businessUser",
+        required: true,
+        type: "string",
+      },
+      {
         param_key: "workerUser",
         required: true,
         type: "string",
@@ -33,7 +48,11 @@ router.post(
         required: true,
         type: "string",
       },
-
+      {
+        param_key: "creatorUser",
+        required: true,
+        type: "string",
+      },
       {
         param_key: "startTime",
         required: true,
@@ -99,5 +118,232 @@ router.put(
   ),
   updateOne
 );
+
+// get day
+router.get("/client/day",
+
+  validateParams(
+    [
+      {
+        param_key: "date",
+        required: true,
+        type: "string",
+      },
+      {
+        param_key: "page",
+        required: false,
+        type: "string",
+      },
+      {
+        param_key: "limit",
+        required: false,
+        type: "string",
+      },
+    ],
+    "query"
+  ),
+  getDayClientId
+)
+
+/* get month */
+router.get("/client/month",
+validateParams(
+  [
+    {
+      param_key: "date",
+      required: true,
+      type: "string",
+    },
+    {
+      param_key: "page",
+      required: false,
+      type: "string",
+    },
+    {
+      param_key: "limit",
+      required: false,
+      type: "string",
+    },
+  ],
+  "query"
+),
+  getMonthClientId
+)
+
+/* get all clients */
+router.get("/clients/allclients",
+  validateParams(
+    [
+
+      {
+        param_key: "page",
+        required: false,
+        type: "string",
+      },
+      {
+        param_key: "limit",
+        required: false,
+        type: "string",
+      },
+    ],
+    "params"
+
+  ),
+  getAllClientsId
+)
+
+/* get workers */
+router.get("/worker/day",
+  validateParams(
+    [
+      {
+        param_key: "date",
+        required: true,
+        type: "string",
+      },
+      {
+        param_key: "page",
+        required: false,
+        type: "string",
+      },
+      {
+        param_key: "limit",
+        required: false,
+        type: "string",
+      },
+
+    ],
+    "query"
+  ),
+  getDayWorkers
+)
+
+/* get allworkers  */
+router.get("/worker/allworkers",
+  validateParams(
+    [
+
+      {
+        param_key: "page",
+        required: false,
+        type: "string",
+      },
+      {
+        param_key: "limit",
+        required: false,
+        type: "string",
+      },
+    ],
+    "params"
+
+  ),
+  getAllworkersId)
+
+/* get specific place */
+router.get("/business/day",
+
+  validateParams(
+    [
+      {
+        param_key: "date_start",
+        required: true,
+        type: "string",
+      },
+
+      {
+        param_key: "date_end",
+        required: true,
+        type: "string",
+      },
+      {
+        param_key: "page",
+        required: false,
+        type: "string",
+      },
+      {
+        param_key: "limit",
+        required: false,
+        type: "string",
+      },
+    ],
+    "query"
+  ),
+
+  getDayBusiness
+
+)
+
+/* get all business */
+router.get("/business/allbusiness",
+  validateParams(
+    [
+
+      {
+        param_key: "page",
+        required: false,
+        type: "string",
+      },
+      {
+        param_key: "limit",
+        required: false,
+        type: "string",
+      },
+    ],
+    "query"
+  ),
+  getAllBusiness
+)
+
+/* get year business */
+router.get("/business/year",
+
+  validateParams(
+    [
+      {
+        param_key: "date",
+        required: true,
+        type: "string",
+      },
+      {
+        param_key: "page",
+        required: false,
+        type: "string",
+      },
+      {
+        param_key: "limit",
+        required: false,
+        type: "string",
+      },
+    ],
+    "query"
+  ),
+  getYearBusiness
+)
+
+/* get month business */
+router.get("/business/month",
+
+  validateParams(
+    [
+      {
+        param_key: "date",
+        required: true,
+        type: "string",
+      },
+      {
+        param_key: "page",
+        required: false,
+        type: "string",
+      },
+      {
+        param_key: "limit",
+        required: false,
+        type: "string",
+      },
+    ],
+    "query"
+  ),
+  getMonthBusiness
+)
 
 export default router;
