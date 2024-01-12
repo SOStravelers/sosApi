44604bec829cbad2f91da48f9018890d31bec318
JohannSch <jschacosta@gmail.com>
2024-01-12 02:56:29 -0300

    cambios

--- src/controllers/notification.js
+++ src/controllers/notification.js
@@ -56,13 +56,11 @@ export const setIsRead = async (req, res, next) => {
 //funcion para saber si un usuario tiene notificaciones sin leer (is Read = false)
 export const checkNotification = async (req, res) => {
   global.logger.info("=== CHECK NOTIFICATION ===");
-  console.log(req.user._id.toString());
   const userId = req.user._id.toString(); // o quizás req.user._id, dependiendo de tu configuración
   const notifications = await Notification.find({
     to: userId,
     isRead: false,
   }).exec();
-  console.log(notifications);
   const result = notifications.length > 0;
   return res.send(result);
 };
