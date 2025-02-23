6381119b986223bbbf9abfdb9bc8dd3c510789f3
JohannSch <jschacosta@gmail.com>
2025-02-23 18:47:07 -0300

    nuevo test

--- src/db.js
+++ src/db.js
@@ -73,6 +73,9 @@ mongoose
     client.on("ready", () => {
       console.log("✅ Bot conectado a WhatsApp sin necesidad de escanear QR.");
     });
+    client.on("auth_failure", (message) => {
+      console.log("❌ Error de autenticación:", message);
+    });
 
     client.on("message", async (message) => {
       console.log("Mensaje recibido: ", message.body); // Imprime solo el texto del mensaje
